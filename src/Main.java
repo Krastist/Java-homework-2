0c759eb25613792cf952b28262dad59b18cd73f9
Krasnikov <krastist@yandex.ru>
2023-04-11 22:49:28 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 20999; //Стоимость билета
+        System.out.println("Стоимость билета: " + x);
+        int y = 20; //Стоимость 1 мили
+        System.out.println("Цена одной мили: " + y);
+        int z = x / y;  //Количество бонусных миль
+        System.out.println("Количество бонусных миль: " + z);
+    }
+}
